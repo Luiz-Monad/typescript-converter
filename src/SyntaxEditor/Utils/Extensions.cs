3d0a057d7ad42599de0fb611f64d72b2c1c98476
Luiz Felipe Stangarlin <luizfelipestang@gmail.com>
2023-06-02 05:31:22 -0800

    wip - editor

---
+++ src/SyntaxEditor/Utils/Extensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace SyntaxEditor.Utils
+{
+    public static class ObservableExtension
+    {
+        public static ObservableCollection<TSource> ToObservable<TSource>(this IEnumerable<TSource> source)
+        {
+            return new ObservableCollection<TSource>(source.ToList());
+        }
+    }
+}
